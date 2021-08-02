1acdfc7a9b6e15b6b4563ed0aa375351cd21cb1e
Billy B <wrbernardoni@gmail.com>
2021-08-02 10:54:13 -0400

    Make engine take color as a parameter

--- cpp_src/utilities/EngineBase.h
+++ cpp_src/utilities/EngineBase.h
@@ -12,22 +12,22 @@ namespace WRB_Chess
 		short scanRec;
 		WRB_Chess::Move moveRec;
 	public:
-		virtual double EvaluatePosition(const WRB_Chess::Bitboard&) = 0;
+		virtual double EvaluatePosition(const WRB_Chess::Bitboard&, WRB_Chess::Color) = 0;
 
-		virtual short RecommendScan(const std::unordered_set<WRB_Chess::Bitboard, WRB_Chess::BoardHash>&) = 0;
-		virtual void RecommendScanAsync(const std::unordered_set<WRB_Chess::Bitboard, WRB_Chess::BoardHash>& brds)
+		virtual short RecommendScan(const std::unordered_set<WRB_Chess::Bitboard, WRB_Chess::BoardHash>&, WRB_Chess::Color) = 0;
+		virtual void RecommendScanAsync(const std::unordered_set<WRB_Chess::Bitboard, WRB_Chess::BoardHash>& brds, WRB_Chess::Color c)
 		{
-			scanRec = RecommendScan(brds);
+			scanRec = RecommendScan(brds, c);
 		}
 		virtual short GetScanRecommendation()
 		{
 			return scanRec;
 		}
 
-		virtual WRB_Chess::Move RecommendMove(const std::unordered_set<WRB_Chess::Bitboard, WRB_Chess::BoardHash>&) = 0;
-		virtual void RecommendMoveAsync(const std::unordered_set<WRB_Chess::Bitboard, WRB_Chess::BoardHash>& brds)
+		virtual WRB_Chess::Move RecommendMove(const std::unordered_set<WRB_Chess::Bitboard, WRB_Chess::BoardHash>&, WRB_Chess::Color) = 0;
+		virtual void RecommendMoveAsync(const std::unordered_set<WRB_Chess::Bitboard, WRB_Chess::BoardHash>& brds, WRB_Chess::Color c)
 		{
-			moveRec = RecommendMove(brds);
+			moveRec = RecommendMove(brds, c);
 		}
 		virtual WRB_Chess::Move GetMoveRecommendation()
 		{
