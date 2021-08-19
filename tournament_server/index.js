b4f18dc46b83764d5993676e80b80bfb40dd8a57
wrbernardoni <wrbernardoni@gmail.com>
2021-08-19 16:31:21 -0400

    Have colors go right between red and green

--- tournament_server/index.js
+++ tournament_server/index.js
@@ -207,18 +207,9 @@ app.get('/', function(req, res) {
 					let g = 0;
 					let b = 0;
 
-					if (wr < 0.5)
-					{
-						r = (1.0 - wr) * 223 + wr * 221
-						g = (1.0 - wr) * 72 + wr * 227
-						b = (1.0 - wr) * 72 + wr * 233
-					}
-					else
-					{
-						r = (1.0 - wr) * 221 + wr * 94
-						g = (1.0 - wr) * 227 + wr * 223
-						b = (1.0 - wr) * 233 + wr * 72
-					}
+					r = (1.0 - wr) * 223 + wr * 94
+					g = (1.0 - wr) * 72 + wr * 223
+					b = (1.0 - wr) * 72 + wr * 72
 					ret += `<td style=\"background-color:rgb(${r},${g},${b});text-align: center\">`
 				}
 				else
